1777472e7769a3d1ede1795607728bac39a022ef
Jefferson Frederyco <jeffersontopmacapa@gmail.com>
2026-07-16 19:10:36 -0300

    chore: inicializa projeto Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="min-h-screen">
+      <h1>Cadastro de Clientes</h1>
+      <p>Em construção.</p>
+    </main>
+  );
+}
+
+export default App;
--- teste-01.js
+++
@@ -1,3 +0,0 @@
-if(confirm('Deseja continuar, com o progresso?')){
-    alert('Você clicou para seguir com o progresso');
-}
